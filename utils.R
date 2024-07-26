fe8fdfd8bdce2bbc4cfa2afccfd6ace5a556daf3
jotasaraiva <jpsaraiva2002@gmail.com>
2024-07-25 23:34:54 -0300

    Adds quarto

--- utils.R
+++ utils.R
@@ -1,33 +1,47 @@
-library(tidyverse)
-library(sf)
-library(leaflet)
-library(forecast)
 
+plot_estratos <- function(df, estratos) {
+  plot <-
+    df |>
+    filter(year(Data_Medicao) == 2024) |>
+    filter(Estrato == estratos) |>
+    ggplot(aes(Data_Medicao, `DAP (cm)`)) +
+    geom_line(aes(group = Id_Amostra, color = Id_Amostra), show.legend = F) +
+    geom_point(
+      aes(
+        group = Id_Amostra,
+        text = sprintf(
+          "Amostra: %s<br>MatGen: %d <br>Data: %s<br>MAC: %s<br>DAP (cm): %f<br>Altura (m): %f",
+          Id_Amostra,
+          Material_Genetico,
+          Data_Medicao,
+          MAC,
+          `DAP (cm)`,
+          `Altura (m)`
+        )
+      ),
+      color = "grey20",
+      size = 1
+    ) +
+    theme_minimal() +
+    labs(title = estratos, x = "Data da Medição")
+  
+  return(plot)
+}
 
-data <- read_csv2("data/Inventario-SingleTrees -- Exportado - 24-07-2024 13-41.csv")
-
-df <-
-  data |> 
-  mutate(across(c(Data_Plantio, Data_Medicao), dmy))
-
-points <- 
-  data |> 
-  count(Id_Amostra, Coordenada_X, Coordenada_Y) |> 
-  st_as_sf(coords = c("Coordenada_X", "Coordenada_Y"))
-
-leaflet(points) |> 
-  addTiles() |> 
-  addMarkers()
-
-# dados com datas de medição anômalas
-df |> 
-  filter(year(Data_Medicao) != 2024) |> 
-  view()
-
-df |> 
-  filter(year(Data_Medicao) == 2024) |> 
-  filter(!Dap_Estimado) |> 
-  filter(Estrato == unique(Estrato)[10]) |> 
-  ggplot(aes(Data_Medicao, `DAP (cm)`, color = Id_Amostra)) +
-  geom_point(aes(group = Id_Amostra))
-
+tab_pretty <- function(data) {
+  data |>
+    tab_style(
+      style = cell_text(align = "center"),
+      locations = cells_stubhead()) |>
+    tab_style(
+      style = cell_text(align = "center"),
+      locations = cells_column_labels()
+    ) |>
+    tab_options(
+      column_labels.background.color = "grey20",
+      column_labels.font.weight = "bold",
+      # row_group.background.color = "#F6F6F6"
+    ) |> 
+    fmt_number(decimals = 2, dec_mark = ",", sep_mark = ".") |> 
+    sub_missing(missing_text = "-")
+}
